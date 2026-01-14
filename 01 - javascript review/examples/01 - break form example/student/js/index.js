3b967e39c48fb1bbc074a7b61b71d8079e31102c
GabbyPotPot <sportugal1@nait.ca>
2026-01-14 11:29:46 -0700

    jan 14

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -1,8 +1,48 @@
 /*
 Enter JS here
 
+
 HTML for list topic list item
 <li class="list-group-item">
     NEW TOPIC HERE
 </li>
 */
+
+// 1. select topic and new topic form
+let topicList = document.querySelector(".topics-list")
+let newTopicForm = document.querySelector(".new-topic-form")
+
+// 5. create function addTopicPage that will take a name and topicListElement
+const addTopicPage = (topicName, topicListElement) => {
+    
+    // 6. create a new inner list element and update the html
+    let newTopicElement = `<li class="list-group-item">
+        ${topicName}
+     </li>`
+
+     topicListElement.innerHTML += newTopicElement
+}
+
+
+// 2. add event listner, stop default form submit event 
+newTopicForm.addEventListener(
+    "submit",
+    (event) => {
+        event.preventDefault()
+        
+        // 3. grab input element and extract/store value
+        let topicInput = event.target.elements["new-topic"]
+        let newTopic = topicInput.value
+        console.log(newTopic)
+        
+        // 4. validation - check for empty, use bootstrap classes for user feedback
+        if (newTopic === "") {
+            topicInput.classList.add("is-invalid")
+        } else {
+            topicInput.classList.remove("is-invalid")
+        }
+        // 6b. call function to event listner
+        addTopicPage(newTopic, topicList)
+    }
+)
+
